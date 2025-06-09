dc64068a90c20fa45d4b78301c5014ae3d11b550
Exploit_Master12 <viaangothivrekar@gmail.com>
2025-06-08 20:24:41 -0700

    Update browser.bash

--- browser.bash
+++ browser.bash
@@ -9,3 +9,15 @@ sudo apt downgrade
 npm i
 npm  load
 //load opera dev github clone using bash linux terminal
+sudo snap install opera
+sudo apt update
+npm i
+npm start
+//load opera with most efficiency as possible
+//load opera using bash linux terminal.
+sudo snap install opera dev
+sudo apt downgrade
+npm i
+npm  load
+//load opera dev github clone using bash linux terminal
+
